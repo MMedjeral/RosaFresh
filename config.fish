ee8033b3f5738970be4ef249fbbddaf38b910d3f
MMedjeral <103764125+MMedjeral@users.noreply.github.com>
2022-04-15 17:49:31 +0200

    Update config.fish

--- config.fish
+++ config.fish
@@ -1,10 +1,7 @@
 # config.fish
 alias lf="ls -aF"
 alias la="ls -la"
-#alias up="sudo pacman -Syuuu"
 alias up="sudo dnf --refresh upgrade"
-#alias poweroff="sudo systemctl poweroff"
-#alias reboot="sudo systemctl reboot"
 
 set fish_greeting
 set -x fish_home ~/.config/fish
