34205b86a35d4f46f715628f9d731e3956112d72
MMedjeral <103764125+MMedjeral@users.noreply.github.com>
2022-04-15 17:48:47 +0200

    Add files via upload

---
+++ config.fish
@@ -0,0 +1,16 @@
+# config.fish
+alias lf="ls -aF"
+alias la="ls -la"
+#alias up="sudo pacman -Syuuu"
+alias up="sudo dnf --refresh upgrade"
+#alias poweroff="sudo systemctl poweroff"
+#alias reboot="sudo systemctl reboot"
+
+set fish_greeting
+set -x fish_home ~/.config/fish
+set -x LANG 'es_ES.UTF-8'
+set -x LC_COLLATE C
+set -x EDITOR vim
+set -x VISUAL vim
+set TERM "xterm-256color"
+
